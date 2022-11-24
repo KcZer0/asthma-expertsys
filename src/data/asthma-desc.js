81f7111efd23837d5d6c275eb58a13dfc4065025
Ploeroe <raphael.hakim@student.umn.ac.id>
2022-11-24 13:09:34 +0700

    Update asthma-desc.js

--- src/data/asthma-desc.js
+++ src/data/asthma-desc.js
@@ -1,15 +1,15 @@
 const asthma = [
   {
     name: 'Intermittent (T1)',
-    desc: '',
+    desc: 'Sediakan Inhalasi agonis beta-2 dirumah atau sebagai alternatifnya kombinasi oral agonis beta-2 dan teofilin. Tempat pengobatan dapat Di rumah atau di praktek dokter/klinik/puskesman',
   },
   {
     name: 'Mild Persistent (T2)',
-    desc: '',
+    desc: 'Sediakan Inhalasi agonis beta-2 dirumah atau sebagai alternatifnya kombinasi oral agonis beta-2 dan teofilin. Tempat pengobatan dapat Di rumah atau di praktek dokter/klinik/puskesman',
   },
   {
     name: 'Moderate Persistent (T3)',
-    desc: '',
+    desc: 'Sediakan Nebulisasi agonis beta-2 dan gunakan setiap 4 jam atau sebagai alternatifnya kombinasi agonis beta-2 subkutan, aminofilin IV, adrenalin 1/1000 0,3ml SK dan bila mungkin gunakan Oksigen Kortikosteroid Sistemik. Tempat pengobatan dapat dilakukan di Unit Gawat Darurat/Rumah Sakit Klinik Praktek dokter Puskesmas',
   },
 ];
 export default asthma;
